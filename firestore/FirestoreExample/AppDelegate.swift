648f371fd0ddc7d110421d73ff8dba412c1dae3b
Ehsan Nasiri <ehsannas@gmail.com>
2022-02-15 11:35:01 -0600

    Testing App Check.

--- firestore/FirestoreExample/AppDelegate.swift
+++ firestore/FirestoreExample/AppDelegate.swift
@@ -16,6 +16,8 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
+import FirebaseAppCheck
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,7 +26,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication
                      .LaunchOptionsKey: Any]?) -> Bool {
+    let providerFactory = AppCheckDebugProviderFactory()
+    AppCheck.setAppCheckProviderFactory(providerFactory)
     FirebaseApp.configure()
+    Firestore.enableLogging(true)
     return true
   }
 }
